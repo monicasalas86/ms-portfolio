e8e93ffb8fb70e76f88c540daaafaccb9bac8870
monicasalas86 <monicsalas167@gmail.com>
2021-10-14 18:02:07 -0500

    fixed a bug

--- src/components/about.js
+++ src/components/about.js
@@ -9,8 +9,8 @@ const About = () => {
             </div>
             <div>
                 <h3>where to find me:</h3>
-                <a href='linkedin.com/in/monicasalas167' target='_blank'>linkedIn</a>
-                <a href='github.com/monicasalas86' target='_blank'>github</a>
+                <a href='www.linkedin.com/in/monicasalas167' target='_blank'>linkedIn</a>
+                <a href='www.github.com/monicasalas86' target='_blank'>github</a>
             </div>
         </div>
         
