51781343fc42961cd87fbdffbfc9a33c7c563f99
monicasalas86 <monicsalas167@gmail.com>
2021-10-14 17:57:18 -0500

    added intro and bio

---
+++ src/components/about.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <div>
+                <h2>a little bit about me...</h2>
+                <p>i am an aspiring full stack developer with dreams of working in greenTech and UX.</p>
+            </div>
+            <div>
+                <h3>where to find me:</h3>
+                <a href='linkedin.com/in/monicasalas167' target='_blank'>linkedIn</a>
+                <a href='github.com/monicasalas86' target='_blank'>github</a>
+            </div>
+        </div>
+        
+    )
+}
+
+export default About;
