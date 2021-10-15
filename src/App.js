99f35f1fdc06bf80d013d67a42edae43cc5df91f
monicasalas86 <monicsalas167@gmail.com>
2021-10-15 01:23:15 -0500

    finished styling porfolio

--- src/App.js
+++ src/App.js
@@ -12,17 +12,26 @@ const App = () => {
     <main>
       <header>
           <nav>
+            <Link to='/' className='navlink'>home</Link>
             <Link to='/about' className='navlink'>about</Link>
             <Link to='/skills' className='navlink'>skills</Link>
           </nav>
       </header>
       <div className='portfolioBody'>
-        <div className='intro'>
+        <div className='intro' path='/'>
           <h1>hey there, i'm monica 👋🏼</h1>
           <p>a full stack web development student at lambda school</p>
-          <About path='/about'/>
-          <Skills path='/skills'/>
         </div>
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/skills'>
+            <Skills />
+          </Route>
+        
+        
+        </Switch>
       </div>
     </main>
   );
